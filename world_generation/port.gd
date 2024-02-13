82ace1257b0202a27bd486158b8d79d340f5cb39
trenton-coggeshall <90164941+trenton-coggeshall@users.noreply.github.com>
2024-02-12 18:42:38 -0800

    Started implementation of transaction calculation

--- world_generation/port.gd
+++ world_generation/port.gd
@@ -7,20 +7,27 @@ var prices : Dictionary
 var demand : Dictionary
 var production : Dictionary
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	initialize()
+	calculate_transaction(EconomyGlobals.GoodType.FOOD, 100)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-func price_check(good):
+# Returns the price of a good at a certain quantity
+func price_check(good, quantity):
 	var base_price = EconomyGlobals.base_prices[good]
 	var flux = float(goods[good] + 1) / float(demand[good])
-	prices[good] = int(round(base_price / flux))
+	
+	return int(round(base_price / flux))
+
+
+# Sets the price of a good based on the current quantity held by the port
+func set_price(good):
+	prices[good] = price_check(good, goods[good])
 
 
 func initialize():
@@ -29,4 +36,18 @@ func initialize():
 	for good in EconomyGlobals.GoodType.values():
 		goods[good] = randi_range(ranges[good][0], ranges[good][1])
 		demand[good] = randi_range(ranges[good][0], ranges[good][1])
-		price_check(good)
+		set_price(good)
+
+
+func calculate_transaction(good, quantity):
+	print("Starting price: " + str(prices[good]))
+	var cost = 0
+	for i in range(quantity):
+		cost += prices[good]
+		goods[good] -= 1
+		set_price(good)
+	
+	print("Ending price: " + str(prices[good]))
+	print("Total cost: " + str(cost))
+
+
