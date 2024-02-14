f4f5ff44e408dc071f1d13691a277250a0288325
Jacob <silverlock99@gmail.com>
2024-02-13 20:04:35 -0800

    fix ui :D

--- playerui.gd
+++ playerui.gd
@@ -1,9 +1,11 @@
 extends Control
 
+@export var speedLabel:Label
+@export var usernameLabel:Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -11,3 +13,15 @@ func _process(delta):
 
 func _on_PlayerShip_speed_changed(speed):
 	pass
+
+func hide_UI():
+	$CanvasLayer.visible = false
+	
+func show_UI():
+	$CanvasLayer.visible = true
+
+func update_speed(value):
+	speedLabel.text = str(value)
+
+func update_username(value):
+	usernameLabel.text = value
