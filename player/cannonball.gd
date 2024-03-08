36fc29185d69ab392d123ae9721fc04c28e5599f
Jacob <silverlock99@gmail.com>
2024-03-07 16:10:05 -0800

    Reorganize cannonball

--- player/cannonball.gd
+++ player/cannonball.gd
@@ -1,14 +1,20 @@
 extends Area2D
 
-var speed = 10000
-#var velocity = Vector2.ZERO
+var speed = 1000
+var velocity = Vector2.ZERO
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += transform.x * speed * delta
+	var direction = Vector2.RIGHT.rotated(rotation)
+	global_position += direction * speed * delta
 
 func _on_body_entered(body):
 	if body.is_in_group("enemy"):
 		#body.queue_free()
 		print("Hit enemy")
 	queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	#queue_free()
+	pass
