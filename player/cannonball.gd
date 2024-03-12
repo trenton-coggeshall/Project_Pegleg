28fb4fab5912b83bc6249780ee9901b32aa8f700
Jacob <silverlock99@gmail.com>
2024-03-12 14:54:16 -0700

    Give cannonball velocity

--- player/cannonball.gd
+++ player/cannonball.gd
@@ -3,10 +3,10 @@ extends Area2D
 var speed = 1000
 var velocity = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
 	global_position += direction * speed * delta
+	global_position += velocity * delta
 
 func _on_body_entered(body):
 	if body.is_in_group("enemy"):
@@ -16,5 +16,4 @@ func _on_body_entered(body):
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	#queue_free()
-	pass
+	queue_free()
