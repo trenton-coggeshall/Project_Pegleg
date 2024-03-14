b9a6b212386d9af32a25f527ccc257eec896ab50
Jacob <silverlock99@gmail.com>
2024-03-14 16:18:26 -0700

    Add more cannons, settings window

--- player/playerui.gd
+++ player/playerui.gd
@@ -6,6 +6,9 @@ extends Control
 @export var healthBar:TextureProgressBar
 @export var damageBar:TextureProgressBar
 
+@export var settingsButton:Button
+@export var settingsWindow:Panel
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Signals.hide_ui.connect(hide_UI)
@@ -42,4 +45,22 @@ func player_damaged(value):
 	
 	tween.tween_property(healthBar, "value", Player.health, 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(damageBar, "value", Player.health, 0.8).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+
+func _on_settings_button_pressed():
+
+	var closedPosition = Vector2(450, -500)
+	var openPosition = Vector2(450, 60)
+	var tween = create_tween()
+	if !settingsWindow.visible: # Open settings
+		settingsWindow.visible = true
+		settingsButton.disabled = true
+		tween.tween_property(settingsWindow, "global_position", openPosition, 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+		await tween.finished
+		settingsButton.disabled = false
+	else: # Close settings
+		settingsButton.disabled = true
+		tween.tween_property(settingsWindow, "global_position", closedPosition, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+		await tween.finished
+		settingsWindow.visible = false
+		settingsButton.disabled = false
 	
