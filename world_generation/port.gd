52e5014e7d75b12f89c94b8fdeee236b25e9c1fb
trenton-coggeshall <90164941+trenton-coggeshall@users.noreply.github.com>
2024-03-18 16:04:42 -0700

    Fixed bug where shooting ports crashed the game

--- world_generation/port.gd
+++ world_generation/port.gd
@@ -121,5 +121,8 @@ func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shap
 
 
 func _on_area_2d_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	if not area:
+		return
+
 	if area.is_in_group("ai_ship"):
 		area.get_parent().current_port = null
