a4fcc40e380b4c5dc3c5902ca2b59d98011173b1
trenton-coggeshall <90164941+trenton-coggeshall@users.noreply.github.com>
2024-04-09 22:45:28 -0700

    Started combat scene/combat player ship

--- combat/combat_scene.gd
+++ combat/combat_scene.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var combat_camera = $CombatCamera
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func start_combat(enemy):
+	combat_camera.make_current()
