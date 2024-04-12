5243f781743799fb4d49c1ae32336320c6a05721
trenton-coggeshall <90164941+trenton-coggeshall@users.noreply.github.com>
2024-04-12 14:53:10 -0700

    Implemented basic combat ai movement

--- combat/combat_scene.gd
+++ combat/combat_scene.gd
@@ -1,11 +1,15 @@
 extends Node
 
 @onready var combat_camera = $CombatCamera
+@onready var combat_player = $CombatPlayer
+
+var player_start
+var ai_start
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player_start = combat_player.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +18,5 @@ func _process(delta):
 
 
 func start_combat(enemy):
+	Player.in_combat = true
 	combat_camera.make_current()
