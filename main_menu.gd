c273c9f340f7f18bb4212089e82fd0a9ab8a7689
trenton-coggeshall <90164941+trenton-coggeshall@users.noreply.github.com>
2024-05-06 16:24:41 -0700

    Make world gen screen more readable

--- main_menu.gd
+++ main_menu.gd
@@ -5,16 +5,6 @@ extends Control
 @onready var settings_screen = $SettingsScreen
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_play_button_pressed():
 	home_screen.hide()
 	world_gen_screen.show()
--- settings_screen.gd
+++ settings_screen.gd
@@ -2,15 +2,6 @@ extends Control
 
 @onready var home_screen = $"../HomeScreen"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_back_button_pressed():
 	hide()
